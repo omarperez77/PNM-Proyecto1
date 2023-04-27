03e1b7a430fe65fbe2e8591927fa23324f71f6a5
juan.jose <juan_camrena@hotmial.com>
2023-04-27 09:11:59 -0600

    vista de SplashScreen

---
+++ app/src/main/java/edu/tecmm/bytebuilders/foodservice/AdminPageRegister.java
@@ -0,0 +1,14 @@
+package edu.tecmm.bytebuilders.foodservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminPageRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page);
+    }
+}
