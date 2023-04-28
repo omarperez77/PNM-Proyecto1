22258c9a7813609a13d26fb8c05024c4e5b7ad46
Omar Pérez <omarg.perezm@gmail.com>
2023-04-28 09:14:17 -0600

    Corrección del SplashScreen

--- app/src/main/java/edu/tecmm/bytebuilders/foodservice/ActivityLogin.java
+++ app/src/main/java/edu/tecmm/bytebuilders/foodservice/ActivityLogin.java
@@ -1,67 +1,13 @@
 package edu.tecmm.bytebuilders.foodservice;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-<<<<<<< HEAD
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
-=======
-import android.os.Bundle;
-<<<<<<< HEAD
->>>>>>> 52b66d5e45ea24eb2fef92a6b7054aa3e297dd54
-=======
->>>>>>> 438aec8a6bbea04ed3ac832a16be7a60706876d6
->>>>>>> camarena
 
 public class ActivityLogin extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/edu/tecmm/bytebuilders/foodservice/MainActivity.java
-<<<<<<< HEAD
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-        // Agregar animaciones
-        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
-        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
 
-
-        TextView codeliaTextView = findViewById(R.id.codeliaTextView);
-        ImageView logoImgeView = findViewById(R.id.logoImagenView);
-
-
-        codeliaTextView.setAnimation(animacion2);
-        logoImgeView.setAnimation(animacion1);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this,
-                        LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 4000);
 
     }
-
 }
-=======
-        setContentView(R.layout.activity_main);
-=======
-        setContentView(R.layout.activity_login);
->>>>>>> dev-manuel:app/src/main/java/edu/tecmm/bytebuilders/foodservice/ActivityLogin.java
-    }
-}
-<<<<<<< HEAD
->>>>>>> 52b66d5e45ea24eb2fef92a6b7054aa3e297dd54
-=======
->>>>>>> 438aec8a6bbea04ed3ac832a16be7a60706876d6
->>>>>>> camarena
---
+++ app/src/main/java/edu/tecmm/bytebuilders/foodservice/MainActivity.java
@@ -0,0 +1,44 @@
+package edu.tecmm.bytebuilders.foodservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        // Agregar animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+
+        TextView codeliaTextView = findViewById(R.id.codeliaTextView);
+        ImageView logoImgeView = findViewById(R.id.logoImagenView);
+
+
+        codeliaTextView.setAnimation(animacion2);
+        logoImgeView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,
+                        LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
