82aa8c3f7bf7d4a170964d3460f9d5568aa73240
Juan Manuel Lopez Galeana <lopezgaleanapelon@gmail.com>
2023-05-02 11:56:33 -0600

    Actualizacion del ActivityLogin

--- app/src/main/java/edu/tecmm/bytebuilders/foodservice/ActivityLogin.java
+++ app/src/main/java/edu/tecmm/bytebuilders/foodservice/ActivityLogin.java
@@ -2,12 +2,31 @@ package edu.tecmm.bytebuilders.foodservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityLogin extends AppCompatActivity {
+
+    private TextView lblGotoRegister;
+    private Button Login;
+    private EditText Email;
+    private EditText Pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        Email = (EditText) findViewById(R.id.txtEmail);
+        Pass = (EditText) findViewById(R.id.txtPass);
+        Login = (Button) findViewById(R.id.btnLogin);
+
+
+    }
+    public void enviar(View view) {
+        String email = Email.getText().toString();
+        String password = Pass.getText().toString();
 
     }
 }
